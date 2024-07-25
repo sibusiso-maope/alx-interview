0c3b2079b23055a5a56bc8b69b64b8b5ae7db750
sibusiso-maope <Sibusiso.Maope@gmail.com>
2024-07-25 11:43:35 -0700

    pascal triangle project fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
 """
-0-pascal_triangle
+Pascal's triangle module
 """
 
+
 def pascal_triangle(n):
+    """ Pascal's triangle generator using addition
+        Args:
+            - n: levels of pascal triangle
+        Return:
+            - Integer matrix representing pascal triangle
     """
-    Returns a list of integers
-    representing the Pascal Triangle of n
-    returns empty list if n <= 0
-    """
-    i = []
     if n <= 0:
-        return i
-    i = [[1]]
-    for k in range(1, n):
-        temp = [1]
-        for l in range(len(i[k - 1]) - 1):
-            curr = i[k - 1]
-            temp.append(i[k - 1][l] + i[k - 1][l + 1])
-        temp.append(1)
-        i.append(temp)
-    return i
+        return []
+
+    pascal_triangle = []
+    prev_row = []
+    for row in range(n):
+        new_row = []
+        for num in range(row + 1):
+            if num == 0:
+                new_row.append(1)
+                continue
+            try:
+                new_row.append(prev_row[num - 1] + prev_row[num])
+            except IndexError:
+                new_row.append(1)
+        prev_row = new_row
+        pascal_triangle.append(new_row)
+    return pascal_triangle
